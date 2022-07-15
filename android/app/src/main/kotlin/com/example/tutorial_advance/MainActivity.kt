b81cdac44e661da6d9597a53f2836be0dbb24c2c
HuzaifaShkx <huzaifayasin225@gmail.com>
2022-07-16 00:36:45 +0500

    Day 2 completed

---
+++ android/app/src/main/kotlin/com/example/tutorial_advance/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tutorial_advance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int a = 30;
+    String s = "Practice Flutter";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Practice Flutter"),
+      ),
+      body: Center(child: Text("welcome to flutter $s in $a days")),
+      drawer: Drawer( ),
+    );
+  }
+}
